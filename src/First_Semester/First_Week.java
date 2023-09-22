dcad368223ec548bf09398bd7681b2542253e104
akif <70863898+akif-ned@users.noreply.github.com>
2023-09-22 21:59:07 +0200

    22/09

---
+++ src/First_Semester/First_Week.java
@@ -0,0 +1,20 @@
+package First_Semester;
+
+public class First_Week {
+
+    public static void main(String[] args) {
+
+       // String yazi= "Javaya Hosgeldin";
+
+        //char a='R';
+        System.out.println("Java yi seviyorum");
+
+        int sayi1=234;
+        int sayi2= 5;
+
+        System.out.println(sayi1);
+
+
+    }
+
+}
