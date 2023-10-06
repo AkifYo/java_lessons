e7e74a3ed7f3297ebdd613459ce770473185271b
akif <70863898+akif-ned@users.noreply.github.com>
2023-10-06 21:32:29 +0200

    06/10

---
+++ src/First_Semester/F03_Third_Week.java
@@ -0,0 +1,23 @@
+package First_Semester;
+
+import java.util.Scanner;
+
+public class F03_Third_Week {
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Lutfen bir adet tam sayi giriniz: ");
+        int number1 =scan.nextInt();
+        System.out.println("Lutfen ikinci tam sayiyi giriniz: ");
+        int number2 =scan.nextInt();
+
+        System.out.println("Girdiginiz 1. sayi: ");
+        System.out.println(number1);
+        System.out.println("Girdiginiz 2. sayi: ");
+        System.out.println(number2);
+    }
+
+
+
+}
---
+++ src/First_Semester/F03_Third_Week_2.java
@@ -0,0 +1,16 @@
+package First_Semester;
+
+import java.util.Scanner;
+
+public class F03_Third_Week_2 {
+    public static void main(String[] args) {
+        // Kullanicidan iki farkli integer deger alarak bu sayilarin toplamini yazdirin.
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Bir tam sayi girer misiniz?");
+        int num1=sc.nextInt();
+        System.out.println("Ikinci bir tam sayi girer misiniz?");
+        int num2= sc.nextInt();
+        int num3=num1+num2;
+        System.out.println(num3);
+    }
+}
