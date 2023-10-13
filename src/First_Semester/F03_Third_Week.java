cd03f4f7ea67f50507b72e83b3c048ecb3b28da8
akif <70863898+akif-ned@users.noreply.github.com>
2023-10-13 21:33:16 +0200

    13/10

--- src/First_Semester/F03_Third_Week.java
+++ src/First_Semester/F03_Third_Week.java
@@ -6,16 +6,21 @@ public class F03_Third_Week {
     public static void main(String[] args) {
 
         Scanner scan= new Scanner(System.in);
+        System.out.println("Adinizi giriniz: ");
+        String userName=scan.next();
 
         System.out.println("Lutfen bir adet tam sayi giriniz: ");
         int number1 =scan.nextInt();
         System.out.println("Lutfen ikinci tam sayiyi giriniz: ");
         int number2 =scan.nextInt();
-
-        System.out.println("Girdiginiz 1. sayi: ");
+        System.out.println("Adiniz ");
+        System.out.println(userName);
+        System.out.println("Girdiginiz "+"    "+ "1.\n sayi: ");
         System.out.println(number1);
         System.out.println("Girdiginiz 2. sayi: ");
         System.out.println(number2);
+
+
     }
 
 
---
+++ src/First_Semester/F04_Fourth_Week.java
@@ -0,0 +1,16 @@
+package First_Semester;
+
+public class F04_Fourth_Week {
+    public static void main(String[] args) {
+        // S07 Patlican medikal centrumda giris memuru olarak,
+        // kayyda gelen hastalarin isim, soyisim ve kimlik numarasi, ve yasini alarak,
+        // asagidaki formule gore odemesi gereken faturayi yazdirin.
+        // Faturada Isim soyisim kinmlik numarasi, yasi ve fiyat olsun.
+        // 18 yas alti hastalar ucretsiz.
+        // 18- 30 yas arasi hastalar %30 Muayane ucreti + %50 Ilac ucreti
+        // 30-50 yas arasi hastalar  %70 Muayene ucreti +  %40 Ilac ucreti
+        // 50 yas ve ustu hastalar  %20 Muayene ucreti + %20 Ilac ucreti odeyecekler.
+        // Tam muayene ucreti 500TL   Tam Ilac ucreti 300TL
+
+    }
+}
