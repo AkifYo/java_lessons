ba1c99ca84efe8f02c04a8053becca43e9a1c01c
akif <70863898+akif-ned@users.noreply.github.com>
2023-10-14 15:29:59 +0200

    14/10

--- src/First_Semester/F04_Fourth_Week.java
+++ src/First_Semester/F04_Fourth_Week.java
@@ -1,16 +1,58 @@
 package First_Semester;
 
+import java.util.Scanner;
+
 public class F04_Fourth_Week {
     public static void main(String[] args) {
         // S07 Patlican medikal centrumda giris memuru olarak,
         // kayyda gelen hastalarin isim, soyisim ve kimlik numarasi, ve yasini alarak,
         // asagidaki formule gore odemesi gereken faturayi yazdirin.
         // Faturada Isim soyisim kinmlik numarasi, yasi ve fiyat olsun.
-        // 18 yas alti hastalar ucretsiz.
-        // 18- 30 yas arasi hastalar %30 Muayane ucreti + %50 Ilac ucreti
-        // 30-50 yas arasi hastalar  %70 Muayene ucreti +  %40 Ilac ucreti
-        // 50 yas ve ustu hastalar  %20 Muayene ucreti + %20 Ilac ucreti odeyecekler.
-        // Tam muayene ucreti 500TL   Tam Ilac ucreti 300TL
+        // Tam muayene ucreti 500TL   KDV 0.18
+
+        //=======================================================
+
+        // ilk olarak scanner clasimizi cagiriyoruz
+        Scanner scan= new Scanner(System.in);
+
+        //Hastane girisi ve super nezaketliyiz.
+        System.out.println("***PATLICAN HASTANESINE HOSGELDINIZ***");
+        System.out.println("LUTFEN ISMINIZI RICA EDEYIM : ");
+        //Burada kisiden ismini string degiskenime atiyorum.
+        String yourName= scan.next();
+
+
+        System.out.println("LUTFEN SOYISMINIZ GIRINIZ : ");
+        //Simdi soyismini string degiskenime atadim.
+        String yourSurname= scan.next();
+
+        System.out.println("KIMLIK NUMARANIZI UNUTMAYALIM LUTFEN : ");
+        //Sahsin kimlik numarasiyla matematiksel bi islem yapmayacagim icin stringe atiyorum.
+        String yourID= scan.next();
+
+
+        System.out.println("VE TABIKI YASINIZ : ");
+        //Kisinin yasini int degiskenimize atiyoruz
+        int yourAge= scan.nextInt();
+
+        //Sonrasind aldigimiz degerleri sahsin faturasina yazdiriyoruz
+        //Muayene ucretimizi de ekliyoruz
+        int treatingPrice=500;
+
+        // Muayene ucretine kdv ekleyelim.
+        double totalPrice= treatingPrice+treatingPrice*(0.18);
+
+        System.out.println("*** PATLICAN HASTENESI FATURASI ***");
+        System.out.println("ISIM =====================> :"+yourName);
+        System.out.println("SOYISIM ==================> :"+yourSurname);
+
+        System.out.println("K. NO ====================> :"+yourID);
+        System.out.println("YASINIZ ==================> :"+yourAge);
+
+        System.out.println("TUTAR ====================> :"+treatingPrice+" TL");
+        System.out.println("TOPLAM TUTAR =============> :"+totalPrice+" TL");
+
+
 
     }
 }
