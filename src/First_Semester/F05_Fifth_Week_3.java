d5e2791e1a0ddc52fe8d2d0597493b742f398b65
akif <70863898+akif-ned@users.noreply.github.com>
2023-10-22 22:40:39 +0200

    22/10

--- src/First_Semester/F05_Fifth_Week_3.java
+++ src/First_Semester/F05_Fifth_Week_3.java
@@ -7,22 +7,21 @@ public class F05_Fifth_Week_3 {
         //Hafsa donem sonu notlarini girip  harf notu olarak hangi puani aldigini
         // gosteren bir program yazmak istemektedir Hafsaya yardimci olalim.
         //  90-100 A,  75-90 B,  60- 75 arasi C, 45-60 D  45 ve asagisi F
-
         Scanner input= new Scanner(System.in);
         System.out.println("Hafsa notun kac? ");
         int notHafsa= input.nextInt();
         if (notHafsa<45){
-            System.out.println("Hafsa notun F kaldin otur");
+            System.out.println("Hafsa notun F kaldin otur.");
         } else if (notHafsa>=45 & notHafsa <60) {
-            System.out.println("Hafsa notun D zorla geciyosun");
+            System.out.println("Hafsa notun D zorla geciyosun.");
         } else if (notHafsa>=60 & notHafsa <75) {
-            System.out.println("Hafsa notun C idare edersin");
+            System.out.println("Hafsa notun C idare edersin.");
         } else if (notHafsa>=75 & notHafsa <90) {
-            System.out.println("Hafsa notun B basarilisin");
+            System.out.println("Hafsa notun B basarilisin.");
         } else if(notHafsa>=90 & notHafsa <=100){
-            System.out.println("Hafsa notun A Onur derecesi aldin");
+            System.out.println("Hafsa notun A onur derecesi aldin TEBRIKLER!");
         } else if (notHafsa<0 & notHafsa >100){
-            System.out.println("Hatali giris yaptiniz");
+            System.out.println("Hatali giris yaptiniz...");
         }
     }
 }
